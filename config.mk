8309c627d78b149b06da562f35ad01071b5c811f
Camille Scholtz <onodera@openmailbox.org>
2016-07-06 18:51:44 +0200

    Update config.mk

--- config.mk
+++ config.mk
@@ -1,4 +1,4 @@
 PREFIX       ?=  /usr
-INSTALL_PROG ?=  install -m755 -D
-INSTALL_FILE ?=  install -m644 -D
+INSTALL_PROG ?=  install -D -m 755
+INSTALL_FILE ?=  install -D -m 644
 RM           ?=  rm -f
