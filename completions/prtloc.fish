d30cf45affc7fe360d8969dbf420c949ce94c35e
Camille Scholtz <onodera@openmailbox.org>
2016-07-07 00:20:40 +0200

    Greatly improve duplicate prtloc listing, move prtlocation into depls for dat speed gain

--- completions/prtloc.fish
+++ completions/prtloc.fish
@@ -1,2 +1,3 @@
 complete -c prtloc -x -a "(prtls | cut -d '/' -f 2)"
+complete -c prtloc -f -o d -l duplicate -d 'list duplicate ports as well'
 complete -c prtloc -f -o h -l help -d 'Print help and exit'
