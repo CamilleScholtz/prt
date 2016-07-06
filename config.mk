a4eb19c2cab1caa8222a71687d537391a593dd24
Camille Scholtz <onodera@openmailbox.org>
2016-07-06 18:55:14 +0200

    remove whitespace

--- config.mk
+++ config.mk
@@ -1,4 +1,4 @@
 PREFIX       ?=  /usr
-INSTALL_PROG ?=  install -D -m 755
-INSTALL_FILE ?=  install -D -m 644
+INSTALL_PROG ?=  install -D -m755
+INSTALL_FILE ?=  install -D -m644
 RM           ?=  rm -f
