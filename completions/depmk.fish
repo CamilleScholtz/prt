c5bbdb8320a159a10174a18e9ac46702f8c3c412
Camille Scholtz <onodera@openmailbox.org>
2016-07-07 18:47:58 +0200

    Use getopts

--- completions/depmk.fish
+++ completions/depmk.fish
@@ -1,3 +1,3 @@
-complete -c depmk -f -o s -l script -d 'Toggle execution of pre- and post-install'
 complete -c depmk -f -o r -l readme -d 'Toggle opening of READMEs'
+complete -c depmk -f -o s -l script -d 'Toggle execution of pre- and post-install'
 complete -c depmk -f -o h -l help -d 'Print help and exit'
