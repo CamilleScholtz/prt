dcc1233994e2cad6365fb9bb711c7f17ac96cc7b
onodera-punpun <onodera@openmailbox.org>
2016-12-24 22:47:24 +0100

    BWAAAHHHWAHAHOOOA

--- runtime/fish/functions/cdp.fish
+++ runtime/fish/functions/cdp.fish
@@ -2,7 +2,7 @@ function cdp
     set portdir (cat /etc/prt/config.toml | string match -r 'portdir.*' | cut -d '=' -f 2 | string trim -c '" ')
     set loc (prt loc $argv ^/dev/null)
 
-    if test "$portdir/$loc"
+    if test "$loc"
         cd $portdir/$loc
     else
         cd $portdir
